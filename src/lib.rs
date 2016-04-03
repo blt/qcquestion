c4ff2a8108994e6e7b226cd0210649b33325849a
Brian L. Troutwine <brian@troutwine.us>
2016-04-02 20:47:45 -0700

    Direct question about static lifetimes, quickcheck.
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,33 @@
 #![cfg_attr(test, feature(plugin))]
 #![cfg_attr(test, plugin(quickcheck_macros))]
 
-#[macro_use]
-extern crate nom;
-
 #[cfg(test)]
 extern crate quickcheck;
 
-pub mod qcq;
+#[cfg(test)]
+use quickcheck::{Arbitrary,Gen};
+
+#[allow(dead_code)]
+#[derive(Debug,Clone,PartialOrd,PartialEq)]
+pub struct C<'a> {
+    s: &'a str,
+    b: bool
+}
+
+#[cfg(test)]
+impl<'a> Arbitrary for C<'a> {
+    fn arbitrary<G: Gen>(g: &mut G) -> C<'a> {
+        let s = g.gen::<&str>();
+        C{s: s, b: (s.len() > 0)}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[quickcheck]
+    fn len_checks_out(c: C) -> bool {
+        (c.s.len() > 0) == c.b
+    }
+}
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate qcq;
-
-fn main() {
-    let rev = qcq::qcq::rev::reverse(&[1,2,3]);
-    println!("{:?}", rev);
-
-    let b = qcq::qcq::rev::parse(b"aaabccc");
-    println!("{:?}", b);
-}
